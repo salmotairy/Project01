5e4dbd580209da211d8cd7cc949a7220c5f87143
salmotairy <s.musaad@gmail.com>
2018-11-12 13:16:07 +0300

    Added cloudinary

--- Gemfile
+++ Gemfile
@@ -6,6 +6,11 @@ ruby '2.5.3'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 gem "devise"
+
+gem 'cloudinary'
+gem 'rest-client', '~> 2.0'
+gem 'carrierwave'
+
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_11_220052) do
+ActiveRecord::Schema.define(version: 2018_11_12_100648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "albums", force: :cascade do |t|
     t.string "name"
-    t.string "key_phot"
+    t.string "key_photo"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
